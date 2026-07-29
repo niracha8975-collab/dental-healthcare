1041aa43ae9ef7177825d8fa4a7e6c1efc5476fb
niracha8975-collab <niracha8975@gmail.com>
2026-07-29 14:16:17 +0700

    Create providers.php

---
+++ backend/backend/backend/backend/bootstrap/providers.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | Providers listed here will be loaded automatically by Laravel.
+    |
+    */
+
+
+];
